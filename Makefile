3fadac9049754aa6383e30ff6c356b8b9aeccd52
Simon Engledew <simon.engledew@osirium.com>
2020-04-24 17:25:16 +0100

    Fix inet_ntoa buffer bug

--- Makefile
+++ Makefile
@@ -1,3 +1,14 @@
-out/ipaddr: src/main.c
+PLATFORM := $(shell uname -s | tr A-Z a-z)
+
+out/$(PLATFORM)/addr: src/main.c
+	mkdir -p $(dir $@)
 	gcc -std=c99 $< -o $@
 	strip $@
+
+ifeq (darwin,$(PLATFORM))
+out/linux/addr: src/main.c
+	docker run -v "$(abspath .):/workspace" -w /workspace --rm $$(docker build -q docker) make $@
+endif
+
+out/addr-%-amd64.tgz: out/%/addr
+	(cd $(dir $<); tar cvzpf $(abspath $@) $(notdir $<))
--- src/main.c
+++ src/main.c
@@ -3,37 +3,55 @@
 #include <string.h>
 #include <arpa/inet.h>
 
+#define hasprefix(s, prefix) strncmp(prefix, s, sizeof prefix - 1) == 0
 
 int main(int argc, char **argv)
 {
-    struct ifaddrs *ifaddrs, *ifaddr;
-    char * address = NULL;
-    char * match = NULL;
+  struct ifaddrs *ifaddrs, *ifaddr;
 
-    if (getifaddrs(&ifaddrs) == 0) {
-        for (ifaddr = ifaddrs; ifaddr != NULL && match == NULL; ifaddr = ifaddr->ifa_next) {
-            int n = 1;
+  char *address = NULL;
+  char *match = NULL;
 
-            do {
-                if (argc == 1 || strcmp(ifaddr->ifa_name, argv[n]) == 0) {
-                    if (ifaddr->ifa_addr == NULL || ifaddr->ifa_netmask == NULL) {
-                        continue;
-                    }
+  static char fallback[16];
 
-                    address = inet_ntoa(((struct sockaddr_in *)ifaddr->ifa_addr)->sin_addr);
+  if (getifaddrs(&ifaddrs) == 0)
+  {
+    for (ifaddr = ifaddrs; ifaddr != NULL && match == NULL; ifaddr = ifaddr->ifa_next)
+    {
+      int n = 1;
 
-                    if (strcmp(address, "0.0.0.0") == 0 || strcmp(address, "127.0.0.1") == 0) {
-                        continue;
-                    }
+      do
+      {
+        if (argc == 1 || strcmp(ifaddr->ifa_name, argv[n]) == 0)
+        {
+          if (ifaddr->ifa_addr == NULL || ifaddr->ifa_netmask == NULL)
+          {
+            continue;
+          }
 
-                    match = address;
-                    break;
-                }
-            } while (++n < argc);
+          address = inet_ntoa(((struct sockaddr_in *)ifaddr->ifa_addr)->sin_addr);
+
+          if (hasprefix(address, "172.16."))
+          {
+            strncpy(fallback, address, sizeof(fallback));
+
+            continue;
+          }
+
+          if (strcmp(address, "0.0.0.0") == 0 || strcmp(address, "127.0.0.1") == 0)
+          {
+            continue;
+          }
+
+          match = address;
+
+          break;
         }
-
-        freeifaddrs(ifaddrs);
+      } while (++n < argc);
     }
 
-    printf("%s\n", match == NULL ? "127.0.0.1" : match);
+    freeifaddrs(ifaddrs);
+  }
+
+  printf("%s\n", match == NULL ? (strlen(fallback) == 0 ? "127.0.0.1" : fallback) : match);
 }
